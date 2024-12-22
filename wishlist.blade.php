f30bcf701766c6c67a74377ec5f2c2bf8325f9e9
bwngan <kieunganworkacc@gmail.com>
2024-12-22 17:54:49 +0700

    Add files via upload

--- wishlist.blade.php
+++ wishlist.blade.php
@@ -43,48 +43,7 @@
                 </div>
                 <aside class="col-left sidebar col-md-3 col-sm-4 col-xs-12 col-sm-pull-8 col-md-pull-9">
                     <div class="side-banner"><img src="{{ asset('frontend/images/side-banner.jpg') }}" alt="banner"></div>
-                    <div class="block block-account">
-                    <div class="block-title">Tài khoản của tôi</div>
-                         <div class="block-content">
-                             <ul>
-                                 <li><a href="#">Trang tổng quan tài khoản</a></li>
-                                 <li><a href="#">Thông tin tài khoản</a></li>
-                                 <li><a href="#">Sổ địa chỉ</a></li>
-                                 <li><a href="#">Đơn hàng của tôi</a></li>
-                                 <li><a href="#">Thỏa thuận thanh toán</a></li>
-                                 <li><a href="#">Hồ sơ định kỳ</a></li>
-                                 <li><a href="#">Đánh giá sản phẩm của tôi</a></li>
-                                 <li><a href="#">Thẻ của tôi</a></li>
-                                 <li class="current"><a href="#">Danh sách yêu thích của tôi</a></li>
-
-                                 <li><a href="#">Có thể tải xuống của tôi</a></li>
-                                 <li class="last"><a href="#">Đăng ký nhận bản tin</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="khối khối-so sánh">
-                         <div class="block-title "><span>So sánh sản phẩm (2)</span></div>
-                         <div class="block-content">
-                             <ol id="compare-items">
-                                 <li class="item lẻ">
-                                     <input type="hidden" value="2173" class="compare-item-id">
-                                     <a class="btn-remove1" title="Xóa mục này" href="#"></a> <a href="#"
-                                         class="product-name"> Ghế sofa có đệm bọc Polyester viền mép</a>
-                                 </li>
-                                 <li class="item last even">
-                                     <input type="hidden" value="2174" class="so-item-id">
-                                     <a class="btn-remove1" title="Xóa mục này" href="#"></a> <a href="#"
-                                         class="product-name"> Ghế sofa có đệm bọc Down-Blend Edge</a>
-                                 </li>
-                             </ol>
-                             <div class="ajax-checkout">
-                                 <button type="submit" title="Gửi"
-                                     class="button button-compare"><span>So sánh</span></button>
-                                 <button type="submit" title="Gửi"
-                                     class="button button-clear"><span>Xóa</span></button>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </aside>
             </div>
         </div>
