574652122bc89fc38982d4d3bd9465c79d7351f3
bwngan <kieunganworkacc@gmail.com>
2024-12-22 17:44:23 +0700

    Add files via upload

---
+++ footer.blade.php
@@ -0,0 +1,50 @@
+<footer class="footer">
+            <div class="newsletter-wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="newsletter">
+                                <form>
+                                    <div>
+                                    <h4><span>bản tin</span></h4>
+                                         <input type="text" placeholder="Nhập địa chỉ email của bạn"
+                                             class="input-text" title="Đăng ký nhận bản tin của chúng tôi" id="newsletter1"
+                                             tên="email">
+                                         <button class="btn-dangky" title="Đăng ký"
+                                             type="submit"><span>Đăng ký</span></button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--newsletter-->
+            <div class="footer-middle">
+                <div class="container">
+                    <div class="row">                         
+                         <div class="col-md-3 col-sm-6">
+                             <h4>liên hệ với chúng tôi</h4>
+                             <div class="contacts-info">
+                                 
+                                 <div class="phone-footer"><i class="phone-icon">&nbsp;</i> 0123456789
+                                 </div>
+                                 <div class="email-footer"><i class="email-icon">&nbsp;</i> <a
+                                         href="mailto:abc@magikcommerce.com">Nhom7@gmail.com</a> </div>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-sm-5 col-xs-12 coppyright"> &copy; {{ now()->format('Y') }} Nhóm 7
+                            
+                         </div>
+                         
+                    </div>
+                </div>
+            </div>
+        </footer>
